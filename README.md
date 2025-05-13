e84ae7a5c46f309b2dee4ea8a587eec6c45f4797
Martin Stender <mst@ingenious-technology.dk>
2025-05-13 10:49:26 +0200

    Draft version

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # L6470 Motor Driver Software
 
-This repository contains a software driver implementation (in C) for the L6470 motor driver from STM. 
+## Intro
+This repository contains a software driver implementation (in C) for the L6470 motor driver from STM <sup>[1]</sup>. 
 
 The L6470 device is a fully integrated microstepping motor driver with motion engine and a SPI interface.
 The L6470 device is suitable for driving two-phase bipolar stepper motors with microstepping, and contains 
@@ -8,12 +9,34 @@ an unique control system with support of up to 1/128 step resolution.
 
 The L6470 driver has a standard 8-bit Serial peripheral interface (SPI) to the host microcontroller (always master). 
 The maximum rate is 5Mbit/s. One option for the L6470 is to place it in a SPI daisy chain configuration to spare
-chip select ($\overline(CS)$) pins on the host microcontroller.
-
-The repository currently contains two files. A single source file (L6470.c) and a header file (L6470.h)
+GPIO pins on the host microcontroller for chip select.
 
 The driver software in this repository was tested on a STM NUCLEO-H743ZI2 evaluation board together with the X-NUCLEO-IHM02A1
-expansion board and two compatible stepper motors. he X-NUCLEO-IHM02A1 board is equipped with two L6470 motor drivers in a daisy
+expansion board <sup>[2]</sup> and two compatible stepper motors. The X-NUCLEO-IHM02A1 board is equipped with two L6470 motor drivers in a daisy
 chain configuration.
+
+## Design & implementation
+The design and implementation is kept simple. Hence, it comes with some limitations. The single source file basically contains
+public functions for the various application commands found in <sup>[1]</sup>. Also, a set of public 'utility' functions are provided for
+conversions.
+
+### Limitations
+The implementation does not handle SPI communication. It provides a hook (or callback function) for sending the created SPI commands.
+This is a design choice. It is up to the user whether to make usage for this hook. If used, the application command is send to the 
+specified L6470 device immediately. If unused, an application command is just created (not send), and it is up to the user when to send it.
+This enables the user to send commands to multiple daisy chained L6470 devices simultaneous (or close to).
+
+### ToDo
+- Handle GetStatus response
+- L6470_Config implementation
+- Elaporate on alarms
+
+## Status
+The released version present is only a draft, and will be updated asap.
+
+## References
+[1] <https://www.st.com/en/motor-drivers/l6470.html>
+
+[2] <https://www.st.com/en/ecosystems/x-nucleo-ihm02a1.html>
  
 
---
+++ Src/L6470.h
@@ -0,0 +1,179 @@
+/**
+ * ****************************************************************************
+ * File: L6470.h
+ *
+ * Author: Martin Stender
+ * Company: Ingenious Technology
+ * Created: May 5, 2025
+ * Version: 1.0
+ *
+ * Description:
+ *   This header file provides definitions, declarations, and public function
+ *   for the STM L6470 motor driver.
+ *
+ ******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2025 [Martin Stender]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * *****************************************************************************
+ */
+
+#ifndef __L6470_H_
+#define __L6470_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+ /* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+
+ /* Definitions ---------------------------------------------------------------*/
+#define SET_STEP_MODE_SYNC_EN_BIT     (0x80)
+
+ /* Type definitions ----------------------------------------------------------*/
+ typedef void (*cbL6470_SpiSend)(const uint8_t * const p_tx,
+		 	 	 	 	 	 	 uint8_t const n_tx,
+								 const uint8_t * const p_rx,
+								 uint8_t const n_rx);
+
+typedef struct
+{
+	uint8_t tx_buffer[4];
+	uint8_t rx_buffer[4];
+	cbL6470_SpiSend SpiSend;
+} sL6470_t;
+
+typedef enum
+{
+  ABS_POS_REG = 0x01,
+  EL_POS_REG = 0x02,
+  MARK_REG = 0x03,
+  SPEED_REG = 0x04,
+  ACC_REG = 0x05,
+  DEC_REG = 0x06,
+  MAX_SPEED_REG = 0x07,
+  MIN_SPEED_REG = 0x08,
+  KVAL_HOLD_REG = 0x09,
+  KVAL_RUN_REG = 0x0A,
+  KVAL_ACC_REG = 0x0B,
+  KVAL_DEC_REG = 0x0C,
+  INT_SPEED_REG = 0x0D,
+  ST_SLP_REG = 0x0E,
+  FN_SLP_ACC_REG = 0x0F,
+  FN_SLP_DEC_REG = 0x10,
+  K_THERM_REG = 0x11,
+  ADC_OUT_REG = 0x12,
+  OCD_TH_REG = 0x13,
+  STALL_TH_REG = 0x14,
+  FS_SPD_REG = 0x15,
+  STEP_MODE_REG = 0x16,
+  ALARM_EN_REG = 0x17,
+  CONFIG_REG = 0x18,
+  STATUS_REG = 0x19
+}eL6470_Register_Address_t;
+
+typedef enum {
+  DIR_REVERSE = 0,
+  DIR_FORWARD = 1
+} eL6470_Direction_t;
+
+
+typedef enum {
+  ACT_ABS_POS_RESET = 0,
+  ACT_ABS_POS_MARK = 1,
+} eL6470_ACT_t;
+
+typedef enum
+{
+  FULL_STEP_MODE = 0x00,
+  HALF_STEP_MODE = 0x01,
+  MICRO_STEP_1_4_MODE = 0x02,
+  MICRO_STEP_1_8_MODE = 0x03,
+  MICRO_STEP_1_16_MODE = 0x04,
+  MICRO_STEP_1_32_MODE = 0x05,
+  MICRO_STEP_1_64_MODE = 0x06,
+  MICRO_STEP_1_128_MODE = 0x07,
+}eL6470_Step_Mode_t;
+
+typedef enum
+{
+	OVERCURRENT_ALARM = 0x01,
+	THERMAL_SHUTDOWN_ALARM = 0x02,
+	THERMAL_WARN_ALARM = 0x04,
+	UNDERVOLATAGE_ALARM = 0x08,
+	STALL_DETECTION_A_ALARM = 0x10,
+	STALL_DETECTION_B_ALARM = 0x20,
+	SWITCH_TURN_ON__ALARM = 0x40,
+	WRONG_COMMAND_ALARM = 0x80,
+}eL6470_Alarm_t;
+
+void L6470_Init(sL6470_t* const p_driver, cbL6470_SpiSend SpiSend);
+void L6470_Config(void);
+
+/* L6470 Application Commands */
+void L6470_Nop(sL6470_t* const p_driver);
+void L6470_SetParam(sL6470_t* const p_driver, const eL6470_Register_Address_t param, const uint32_t value);
+void L6470_GetParam(sL6470_t* const p_driver, const eL6470_Register_Address_t param);
+void L6470_Run(sL6470_t* const p_driver, const eL6470_Direction_t dir, const uint32_t speed);
+void L6470_StepClock(sL6470_t* const p_driver, const eL6470_Direction_t dir);
+void L6470_Move(sL6470_t* const p_driver, const eL6470_Direction_t dir, const uint32_t n_step);
+void L6470_GoTo(sL6470_t* const p_driver, const uint32_t abs_pos);
+void L6470_GoTo_DIR(sL6470_t* const p_driver, const eL6470_Direction_t dir, const uint32_t abs_pos);
+void L6470_GoUntil(sL6470_t* const p_driver, const eL6470_ACT_t act, const eL6470_Direction_t dir, const uint32_t speed);
+void L6470_ReleaseSW(sL6470_t* const p_driver, const eL6470_ACT_t act, const eL6470_Direction_t dir);
+void L6470_GoHome(sL6470_t* const p_driver);
+void L6470_GoMark(sL6470_t* const p_driver);
+void L6470_ResetPos(sL6470_t* const p_driver);
+void L6470_ResetDevice(sL6470_t* const p_driver);
+void L6470_SoftStop(sL6470_t* const p_driver);
+void L6470_HardStop(sL6470_t* const p_driver);
+void L6470_SoftHiZ(sL6470_t* const p_driver);
+void L6470_HardHiZ(sL6470_t* const p_driver);
+void L6470_GetStatus(sL6470_t* const p_driver);
+
+/* L6470 utility functions for conversions */
+float L6470_MaxSpeed_2_Step_s(uint16_t const max_speed);
+uint16_t L6470_Step_s_2_MaxSpeed(float const step_s);
+float L6470_Speed_2_Step_s(uint32_t speed);
+uint32_t L6470_Step_s_2_Speed(float step_s);
+float L6470_OcdTh_2_mA(uint8_t ocd_th);
+uint8_t L6470_mA_2_OcdTh(const float mA);
+float L6470_StallTh_2_mA(uint8_t stall_th);
+uint8_t L6470_mA_2_StallTh(const float mA);
+float L6470_Acc_2_Step_s2(uint16_t const acc);
+uint16_t L6470_Step_s2_2_Acc(float const step_s2);
+float L6470_Dec_2_Step_s2(uint16_t const dec);
+uint16_t L6470_Step_s2_2_Dec(float const step_s2);
+float L6470_FsSpd_2_Step_s(uint16_t const fs_spd);
+uint16_t L6470_Step_s_2_FsSpd(float const step_s);
+float L6470_IntSpeed_2_Step_s(uint16_t const int_speed);
+uint16_t L6470_Step_s_2_IntSpeed(float const step_s);
+float L6470_StSlp_2_s_Step(uint8_t const st_slp);
+uint8_t L6470_s_Step_2_StSlp(float const s_step);
+float L6470_FnSlpAcc_2_s_Step(uint8_t const fn_slp_acc);
+uint8_t L6470_s_Step_2_FnSlpAcc(float const s_step);
+float L6470_FnSlpDec_2_s_Step(uint8_t const fn_slp_dec);
+uint8_t L6470_s_Step_2_FnSlpDec(float const s_step);
+
+#endif /* __L6470_H_ */
